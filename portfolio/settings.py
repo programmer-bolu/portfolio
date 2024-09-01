14f3f59b68ae92a3eba6a03b4c5650ba3c8a9c1d
programmer-bolu <akingbadeboluwatife030@gmail.com>
2024-09-01 21:57:09 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'PASSWORD': 'Ilerioluwa1@'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
